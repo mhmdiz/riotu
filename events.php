dabfbdd2654804fe38048c7609d7fbb59bb6db18
mhmdiz <34584708+mhmdiz@users.noreply.github.com>
2017-12-16 22:41:32 +0300

    Events Default
    Default Page

---
+++ events.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Robotics and Internet of Things IoT Unit - Events</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Responsive Website Template">
+<meta name="author" content="The Develovers">
+<!-- CORE CSS -->
+<!-- CORE CSS -->
+<?php
+    include_once 'include/headers_links.php';
+    include_once 'include/utils.php';
+    ?>
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+	<?php
+	include_once 'include/nav_bar.php';
+	?>
+		<!-- END NAVBAR -->
+		<!-- PAGE HEADER DEFAULT -->
+		<div class="page-header">
+			<div class="container">
+				<h1 class="page-title pull-left">Events</h1>
+				<ol class="breadcrumb link-accent">
+					<li><a href="index.php">Home</a></li>
+
+					<li class="active">Has Navigation Links</li>
+				</ol>
+			</div>
+		</div>
+		<!-- END PAGE HEADER DEFAULT -->
+		<!-- PAGE CONTENT -->
+		<div class="page-content no-margin-bottom">
+			<div class="container">
+				<p></p>
+
+				<h2 class="section-heading">Events</h2>
+				<div class="container">
+					<div class="blog-masonry clearfix">
+						<div class="grid-sizer"></div>
+						<div class="grid-item grid-item-width-2x">
+							<div class="post-entry-card post-entry-featured">
+								<a href="#"><img src="images/drone-training.png"
+									class="img-responsive" alt="Post Thumbnail"> </a>
+								<div class="post-info">
+									<h3 class="post-title">
+										<a href="#">Drone Pilot Training Certificate</a>
+									</h3>
+									<p class="post-excerpt">The objective of this training program
+										is to provide a quick start to drone piloting with the MAVLink
+										protocol. Learning how to pilot drone and define autonomous
+										mission is very challenging and time consuming. There is a
+										huge amount of informtaion to know, yet this information is
+										very scattered on the Internet. By attending the 13-hour
+										training sessions, the participant will have a full
+										understanding of drone technologies, and will have the ability
+										to build hos own drone, and enjoy piloting it either with a
+										remote control or using a ground station.</p>
+									<span class="post-meta"><i class="fa fa-calendar-o"></i> March
+										28-30, 2017</span> <a
+										href="http://wiki.coins-lab.org/trainings/drone/index.html"
+										class="read-more pull-right">Read More</a>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+
+
+			</div>
+			<!--
+<div class="cta cta-solid-bg cta-2-columns margin-top-50">
+<div class="container">
+<h2 class="heading">An elegant Bootstrap theme with tons of features</h2>
+<a href="#" class="btn btn-primary btn-lg"><i class="fa fa-shopping-cart"></i> PURCHASE</a>
+</div>
+</div>
+-->
+		</div>
+		<!-- END PAGE CONTENT -->
+		<!-- FOOTER -->
+		<?php
+		include_once 'include/footer.php';
+		?>
+		<!-- END FOOTER -->
+		<div class="back-to-top">
+			<a href="#top"><i class="fa fa-chevron-up"></i> </a>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+	<!-- JAVASCRIPT -->
+	<script src="theme/assets/js/jquery-2.1.1.min.js"></script>
+	<script src="theme/assets/js/bootstrap.min.js"></script>
+	<script src="theme/assets/js/plugins/easing/jquery.easing.min.js"></script>
+	<script src="theme/assets/js/bravana.js"></script>
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54522619-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+</html>
