a0a8a16b6d5496cba0920db79065624a36732b1a
aymanpsu <32359572+aymanpsu@users.noreply.github.com>
2017-12-16 22:12:34 +0300

    Collaboration DEFAULT
    default page

---
+++ collaboration.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Robotics and Internet of Things IoT Unit - News</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Responsive Website Template">
+<meta name="author" content="The Develovers">
+<!-- CORE CSS -->
+<!-- CORE CSS -->
+<?php
+    include_once 'include/headers_links.php';
+    include_once 'include/utils.php';
+    ?>
+</head>
+
+<body>
+<!-- WRAPPER -->
+<div id="wrapper">
+<?php
+    include_once 'include/nav_bar.php';
+    ?>
+<!-- END NAVBAR -->
+<!-- PAGE HEADER DEFAULT -->
+<div class="page-header">
+<div class="container">
+<h1 class="page-title pull-left">News</h1>
+<ol class="breadcrumb link-accent">
+<li><a href="index.php">Home</a></li>
+
+<li class="active">Has Navigation Links</li>
+</ol>
+</div>
+</div>
+<!-- END PAGE HEADER DEFAULT -->
+<!-- PAGE CONTENT -->
+<div class="page-content no-margin-bottom">
+<div class="container">
+<p>  </p>
+
+<h2 class="section-heading">Collaboration</h2>
+<div class="row">
+<div class="col-lg-6">
+<a href='http://www.gaitech.hk'><img src="images/gaitech.png" width="70%"
+					class="image-responsive center"> </a>
+</div>
+<div class="col-lg-6">
+<a href='https://www.cister.isep.ipp.pt/'><img src="images/cister.jpg" width="30%"
+					class="image-responsive center"> </a>
+</div>
+</div>
+</div>
+<!--
+<div class="cta cta-solid-bg cta-2-columns margin-top-50">
+<div class="container">
+<h2 class="heading">An elegant Bootstrap theme with tons of features</h2>
+<a href="#" class="btn btn-primary btn-lg"><i class="fa fa-shopping-cart"></i> PURCHASE</a>
+</div>
+</div>
+-->
+</div>
+<!-- END PAGE CONTENT -->
+<!-- FOOTER -->
+<?php
+    include_once 'include/footer.php';
+    ?>
+<!-- END FOOTER -->
+<div class="back-to-top">
+<a href="#top"><i class="fa fa-chevron-up"></i></a>
+</div>
+</div>
+<!-- END WRAPPER -->
+<!-- JAVASCRIPT -->
+<script src="theme/assets/js/jquery-2.1.1.min.js"></script>
+<script src="theme/assets/js/bootstrap.min.js"></script>
+<script src="theme/assets/js/plugins/easing/jquery.easing.min.js"></script>
+<script src="theme/assets/js/bravana.js"></script>
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54522619-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+</html>
