533bb9e1f66fc0000d2803765325eece61273d11
aymanpsu <32359572+aymanpsu@users.noreply.github.com>
2017-12-17 01:23:57 +0300

    About Default

---
+++ about.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Robotics and Internet of Things IoT Unit - About</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Responsive Website Template">
+<meta name="author" content="The Develovers">
+<!-- CORE CSS -->
+<?php
+include_once 'include/headers_links.php';
+?>
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+	<?php
+	include_once 'include/nav_bar.php';
+	?>
+		<!-- END NAVBAR -->
+		<!-- PAGE HEADER DEFAULT -->
+		<div class="page-header">
+			<div class="container">
+				<h1 class="page-title pull-left">ABOUT</h1>
+				<ol class="breadcrumb link-accent">
+					<li><a href="index.php">Home</a></li>
+
+					<li class="active">Has Navigation Links</li>
+				</ol>
+			</div>
+		</div>
+		<!-- END PAGE HEADER DEFAULT -->
+		<!-- PAGE CONTENT -->
+		<div class="page-content no-margin-bottom">
+			<div class="container">
+				<a href='#'><img src="images/lab04.jpg" width="70%"
+					class="image-responsive center"> </a> <br> <br> <br> 
+				<p class="paragraph_large">One of the initiatives of the National
+					Transformation Program is coming up with a Low Cost Communication
+					Network for the Internet of Things (IoT) applications. IoT
+					applications span several areas of the Saudi Vision, Health, Energy
+					Consumption, Security Monitoring, etc. These applications can be
+					also used in the concept of smart housing, smart campus, and smart
+					workplace. Establishing a professional research environment in the
+					Robotics and Internet of Things (IoT) will contribute to 2030
+					initiative. It will pursue and attract external research funds and
+					grants from industry and external research programs by providing
+					consultation service as well as developing IoT solutions and
+					providing training services for Saudi Market.</p>
+
+				<h2 class="section-heading">VISION</h2>
+				<p class="paragraph_large">To be an internationally recognized
+					centre in robotics and internet of things</p>
+
+				<h2 class="section-heading">MISSION</h2>
+
+				<ul class="paragraph_large">
+					<li>To promote applied research on Robotics and Internet of Things
+						in Prince Sultan University through international collaboration
+						with industrial and academic partners</li>
+					<li>To develop robotics and IoT solutions for real applications to
+						sustain the economic and social development in Saudi Arabia inline
+						with 2030 vision.</li>
+
+				</ul>
+
+
+
+				<h2 class="section-heading">OBJECTIVES</h2>
+
+				<ul class="paragraph_large">
+					<li>Establish a professional research environment in PSU in the
+						robotics and IoT research.</li>
+					<li>Attract external funds and grants to PSU.</li>
+					<li>Provide a consultation service to the industry both locally and
+						internationally.</li>
+					<li>Develop robotics and IoT solution’s for the Saudi Market in
+						collaboration with international companies.</li>
+					<li>Providing training services to communities in the field of
+						robotics and IoT.</li>
+					<li>Contribute to the involvement of undergraduate and graduate
+						students in professional research and promote their intellectual
+						and learning skills.</li>
+					<li>Establish international collaboration with reputed research
+						groups and institutions in robotics and IoT.</li>
+					<li>Contribute to the enrichment of computer science and
+						engineering programs in the field of robotics and IoT</li>
+
+				</ul>
+
+			</div>
+			<!--
+<div class="cta cta-solid-bg cta-2-columns margin-top-50">
+<div class="container">
+<h2 class="heading">An elegant Bootstrap theme with tons of features</h2>
+<a href="#" class="btn btn-primary btn-lg"><i class="fa fa-shopping-cart"></i> PURCHASE</a>
+</div>
+</div>
+-->
+		</div>
+		<!-- END PAGE CONTENT -->
+		<!-- FOOTER -->
+		<?php
+		include_once 'include/footer.php';
+		?>
+		<!-- END FOOTER -->
+		<div class="back-to-top">
+			<a href="#top"><i class="fa fa-chevron-up"></i> </a>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+	<!-- JAVASCRIPT -->
+	<script src="theme/assets/js/jquery-2.1.1.min.js"></script>
+	<script src="theme/assets/js/bootstrap.min.js"></script>
+	<script src="theme/assets/js/plugins/easing/jquery.easing.min.js"></script>
+	<script src="theme/assets/js/bravana.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54522619-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+</body>
+
+</html>
