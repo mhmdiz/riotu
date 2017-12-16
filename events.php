97916275777c10eac766dacf83c7e293c48220da
mhmdiz <34584708+mhmdiz@users.noreply.github.com>
2017-12-16 22:42:52 +0300

    Events v2
    Changing the Events page from static to dynamic.

--- events.php
+++ events.php
@@ -19,6 +19,9 @@
 	<!-- WRAPPER -->
 	<div id="wrapper">
 	<?php
+    include 'include/dbconfig.php';
+    ?>
+	<?php
 	include_once 'include/nav_bar.php';
 	?>
 		<!-- END NAVBAR -->
@@ -45,43 +48,34 @@
 						<div class="grid-sizer"></div>
 						<div class="grid-item grid-item-width-2x">
 							<div class="post-entry-card post-entry-featured">
-								<a href="#"><img src="images/drone-training.png"
-									class="img-responsive" alt="Post Thumbnail"> </a>
-								<div class="post-info">
-									<h3 class="post-title">
-										<a href="#">Drone Pilot Training Certificate</a>
-									</h3>
-									<p class="post-excerpt">The objective of this training program
-										is to provide a quick start to drone piloting with the MAVLink
-										protocol. Learning how to pilot drone and define autonomous
-										mission is very challenging and time consuming. There is a
-										huge amount of informtaion to know, yet this information is
-										very scattered on the Internet. By attending the 13-hour
-										training sessions, the participant will have a full
-										understanding of drone technologies, and will have the ability
-										to build hos own drone, and enjoy piloting it either with a
-										remote control or using a ground station.</p>
-									<span class="post-meta"><i class="fa fa-calendar-o"></i> March
-										28-30, 2017</span> <a
-										href="http://wiki.coins-lab.org/trainings/drone/index.html"
-										class="read-more pull-right">Read More</a>
+								<?php
+						        $sql = "SELECT * FROM events";
+						        $result = $dbconn->query($sql);
+						        if ($result->num_rows >= 1) {
+						          // output data of each row
+						          while($row = $result->fetch_assoc()) {
+						          echo '<a href="#">';
+						          echo "<img src='".$row["event_picture"]."'";
+						          echo 'class="img-responsive" alt="Post Thumbnail">';
+						          echo '</a>';
+						          echo '<div class="post-info">';
+						          echo '<h3 class="post-title">';
+						          echo '<a href="#">'.$row["event_name"].'</a>';
+						          echo '</h3>';
+						          echo '<p class="post-excerpt">'.$row["event_description"].'</p>';
+						          echo '<span class="post-meta"><i class="fa fa-calendar-o">';
+						          echo '</i>'.$row["event_date"].'</span>';
+						          echo "<a href='".$row["event_link"]."'";
+						          echo 'class="read-more pull-right">Read More</a>';
+							      }
+						   	    }
+          						?>
 								</div>
 							</div>
 						</div>
-						
 					</div>
 				</div>
-
-
 			</div>
-			<!--
-<div class="cta cta-solid-bg cta-2-columns margin-top-50">
-<div class="container">
-<h2 class="heading">An elegant Bootstrap theme with tons of features</h2>
-<a href="#" class="btn btn-primary btn-lg"><i class="fa fa-shopping-cart"></i> PURCHASE</a>
-</div>
-</div>
--->
 		</div>
 		<!-- END PAGE CONTENT -->
 		<!-- FOOTER -->
