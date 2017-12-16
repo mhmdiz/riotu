d7891514449b8bb91eb249b83ef405aa84d32caa
aymanpsu <32359572+aymanpsu@users.noreply.github.com>
2017-12-17 01:50:21 +0300

    Research Default

---
+++ research.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Robotics and Internet of Things IoT Unit - Research Area</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Responsive Website Template">
+<meta name="author" content="The Develovers">
+<!-- CORE CSS -->
+<!-- CORE CSS -->
+<?php
+    include_once 'include/headers_links.php';
+    include_once 'include/utils.php';
+    ?>
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+	<?php include_once 'include/nav_bar.php';?>
+
+		<!-- PAGE HEADER DEFAULT -->
+		<div class="page-header">
+			<div class="container">
+				<h1 class="page-title pull-left">Research</h1>
+				<ol class="breadcrumb link-accent">
+					<li><a href="index.php">Home</a></li>
+
+					<li class="active">Has Navigation Links</li>
+				</ol>
+			</div>
+		</div>
+		<!-- END PAGE HEADER DEFAULT -->
+		<!-- PAGE CONTENT -->
+		<div class="page-content no-margin-bottom">
+			<div class="container">
+				<p></p>
+
+				<h2 class="section-heading">RESEARCH AREAS</h2>
+				
+
+				<div class="paragraph_large">
+					The <strong>Robotics and Internet-of-Things</strong> research lab focuses on the
+					integration of robots into the Internet of Things to promote new
+					types of applications that leverage the use of these two
+					technologies. RIOT has a long expertise in developing solutions for
+					Internet-of-Things and Mobile Robots. The research lab also fosters its
+					activites to be aligned with national priorities in particular the
+					2020 National Transformation Plan and 2030 Vision. In particular,
+					with a strategic vision for the future, RIOT is working in emerging
+					topics such as:
+
+<br><br>
+
+					<ul>
+						<li>Software Engineering for Cloud Robotics</li>
+						<li>Big data analytics for IoT and Robotics applications</li>
+						<li>Security and safety of drones</li>
+						<li>Integration of robots/drones into the IoT and the Cloud</li>
+						<li>Communication and Networking for the IoT</li>
+						<li>AI Algorithms for Planning in Robotics</li>
+						<li>Design of Unmanned Aerial Vehicles (UAVs) and their applications</li>
+					</ul>
+				</div>
+				<br>
+				<h2 class="section-heading">CONSULTATION SERVICES</h2>
+				
+<div class="paragraph_large">
+					The <strong>Robotics and Internet-of-Things</strong> provides consultation services to industry, academia and government institutions to develop real-world solutions around our research areas. 
+					We also provide training services on drones building, piloting and applications development, robots software development, Robot Operating System (ROS).
+					For further information, contact us.  
+
+				</div>
+			</div>
+			
+			
+			<!--
+<div class="cta cta-solid-bg cta-2-columns margin-top-50">
+<div class="container">
+<h2 class="heading">An elegant Bootstrap theme with tons of features</h2>
+<a href="#" class="btn btn-primary btn-lg"><i class="fa fa-shopping-cart"></i> PURCHASE</a>
+</div>
+</div>
+-->
+		</div>
+		<!-- END PAGE CONTENT -->
+		<!-- FOOTER -->
+		<?php
+		include_once 'include/footer.php';
+		?>
+		<!-- END FOOTER -->
+		<div class="back-to-top">
+			<a href="#top"><i class="fa fa-chevron-up"></i> </a>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+	<!-- JAVASCRIPT -->
+	<script src="theme/assets/js/jquery-2.1.1.min.js"></script>
+	<script src="theme/assets/js/bootstrap.min.js"></script>
+	<script src="theme/assets/js/plugins/easing/jquery.easing.min.js"></script>
+	<script src="theme/assets/js/bravana.js"></script>
+
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54522619-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+</html>
