5264a34f239e9472d72c6f48ea694891e2261398
mhmdiz <34584708+mhmdiz@users.noreply.github.com>
2017-12-22 21:57:10 +0300

    Index Default
    Default Page

---
+++ index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Robotics and Internet of Things IoT Unit</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Responsive Website Template">
+<meta name="author" content="The Develovers">
+<?php
+include_once 'include/headers_links.php';
+?>
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+	<?php
+	include_once 'include/nav_bar_black.php';
+	?>
+		<!-- END NAVBAR -->
+		<!-- SLIDER -->
+		<section id="slider-fullwidth" class="rev_slider no-padding">
+			<ul>
+				<li data-transition="crossfade"><img
+					src="images/profile-background-psu.jpg" data-bgfit="cover"
+					data-bgposition="center center" data-bgrepeat="no-repeat"
+					alt="Slide">
+					<div class="tp-caption tp-shape tp-shapewrapper " data-width="full"
+						data-height="full" data-basealign="slide"
+						data-transform_in="o:0;s:800;e:Power3.easeInOut;"
+						data-transform_idle="o:1"
+						data-transform_out="o:0;s:1500;e:Power3.easeInOut;" data-start="0">
+					</div>
+					<div
+						class="tp-caption tp-resizeme lead-text light-text text-center"
+						data-x="center" data-y="center" data-hoffset="0"
+						data-voffset="-100" data-transform_in="o:0;y:-20;s:2000;"
+						data-transform_idle="o:1;"
+						data-transform_out="o:0;s:1500;e:Power3.easeInOut;"
+						data-start="1300">
+						<h1 class="heading" style="margin-top: 120px">RIOT Organizes the
+							Second Drone Pilot Certificate Program</h1>
+						<p class="section-lead">at Prince Sultan University, Riyadh,
+							January 2018</p>
+						<a href='http://www.springer.com/us/book/9783319549262'><img
+							src="images/drone-training.png" class="image-responsive center">
+						</a>
+					</div>
+					<div class="tp-caption tp-resizeme" data-x="center" data-y="center"
+						data-hoffset="0" data-voffset="[150, 170, 180, 200]"
+						data-transform_in="o:0;y:50;s:2000;" data-transform_idle="o:1;"
+						data-transform_out="o:0;y:0;s:1500;e:Power3.easeInOut;"
+						data-start="1900" style="margin-top: 100px"
+						onclick="alert('coming soon');">
+						<a href="#" class="btn btn-primary btn-lg"><i
+							class="fa fa-shopping-cart"></i> REGISTER NOW</a>
+					</div></li>
+				<li data-transition="crossfade"><img
+					src="images/profile-background-psu.jpg" data-bgfit="cover"
+					data-bgposition="center center" data-bgrepeat="no-repeat"
+					alt="Slide">
+					<div class="tp-caption tp-shape tp-shapewrapper " data-width="full"
+						data-height="full" data-basealign="slide"
+						data-transform_in="o:0;s:800;e:Power3.easeInOut;"
+						data-transform_idle="o:1"
+						data-transform_out="o:0;s:1500;e:Power3.easeInOut;" data-start="0">
+					</div>
+					<div
+						class="tp-caption tp-resizeme lead-text light-text text-center"
+						data-x="center" data-y="center" data-hoffset="0"
+						data-voffset="-100" data-transform_in="o:0;y:-20;s:2000;"
+						data-transform_idle="o:1;"
+						data-transform_out="o:0;s:1500;e:Power3.easeInOut;"
+						data-start="1300">
+						<h2>Get your copy of the book</h2>
+						<h1 class="heading" style="margin-top: 120px">Robot Operating
+							System (ROS): The Complete Reference (Volume 2)</h1>
+						<a href='http://www.springer.com/us/book/9783319549262'><img
+							src="images/rosbook2.jpg" class="image-responsive center"> </a>
+
+					</div>
+					<div class="tp-caption tp-resizeme" data-x="center" data-y="center"
+						data-hoffset="0" data-voffset="[150, 170, 180, 200]"
+						data-transform_in="o:0;y:50;s:2000;" data-transform_idle="o:1;"
+						data-transform_out="o:0;y:0;s:1500;e:Power3.easeInOut;"
+						data-start="1900" style="margin-top: 100px"
+						onclick="alert('coming soon');">
+						<a href="http://www.springer.com/us/book/9783319549262"
+							class="btn btn-primary btn-lg"><i class="fa fa-shopping-cart"></i>
+							PURCHASE</a>
+					</div></li>
+
+
+			</ul>
+		</section>
+		<!-- END SLIDER -->
+		<!-- CONTENT SLIDER -->
+		<!--
+<section>
+<div class="container">
+<div class="section-heading section-heading-center">
+<h2 class="heading">WHY BRAVANA</h2>
+<p class="section-lead">Appropriately exploit 24/7 leadership
+skills without user friendly leadership.</p>
+</div>
+<div id="content-slider2" class="owl-carousel carousel-content">
+<div>
+<div class="media">
+<div class="media-left">
+<img src="theme/assets/img/content-slider-img2.png" alt="">
+</div>
+<div class="media-body">
+<h2>Multipurpose</h2>
+<p>Quickly myocardinate strategic technologies with
+extensible e-tailers. Competently restore fully researched.</p>
+<p>Quickly re-engineer next-generation services for
+standardized e-services. Continually streamline go forward
+ideas for cooperative interfaces. Collaboratively.</p>
+<p>Quickly optimize open-source e-services with transparent
+communities. Completely enable best-of-breed results with
+mission-critical materials. Uniquely revolutionize user-centric
+human capital vis-a-vis just in time web services. Seamlessly
+strategize client-centric testing procedures before
+user-centric methods of empowerment. Rapidiously pontificate
+cost effective portals after extensible web-readiness.</p>
+<a href="#">Learn More &nbsp;<i class="fa fa-angle-right"></i></a>
+</div>
+</div>
+</div>
+<div>
+<div class="media">
+<div class="media-left">
+<img src="theme/assets/img/content-slider-img3.png" alt="">
+</div>
+<div class="media-body">
+<h2>Predefined Color Skins</h2>
+<p>Synergistically e-enable low-risk high-yield niche
+markets without real-time e-business. Interactively redefine
+24/365 channels after customized convergence. Interactively
+synthesize B2C users after global networks. Phosfluorescently
+whiteboard bricks-and-clicks services after B2B schemas.
+Enthusiastically create leveraged channels after visionary ROI.</p>
+<p>Quickly optimize open-source e-services with transparent
+communities. Completely enable best-of-breed results with
+mission-critical materials. Uniquely revolutionize user-centric
+human capital vis-a-vis just in time web services.</p>
+<a href="#">Learn More &nbsp;<i class="fa fa-angle-right"></i></a>
+</div>
+</div>
+</div>
+<div>
+<div class="media">
+<div class="media-left">
+<img src="theme/assets/img/content-slider-img.png" alt="Bravana">
+</div>
+<div class="media-body">
+<div
+class="icon-info icon-info-left icon-info-big margin-bottom-30">
+<i
+class="icon icon_pencil-edit ico-styled-square-solid bg-primary"></i>
+<div class="text">
+<h2 class="title">Carefully Handcrafted</h2>
+<p>Holisticly harness just in time technologies via
+corporate scenarios. Intrinsicly predominate ubiquitous
+outsourcing through an expanded array of functionalities.</p>
+</div>
+</div>
+<div
+class="icon-info icon-info-left icon-info-big margin-bottom-30">
+<i class="icon icon_mobile ico-styled-square-solid bg-primary"></i>
+<div class="text">
+<h2 class="title">Mobile Friendly</h2>
+<p>Holisticly harness just in time technologies via
+corporate scenarios. Intrinsicly predominate ubiquitous
+outsourcing through an expanded array of functionalities.</p>
+</div>
+</div>
+<div
+class="icon-info icon-info-left icon-info-big margin-bottom-30">
+<i class="icon icon_like ico-styled-square-solid bg-primary"></i>
+<div class="text">
+<h2 class="title">Slider Revolution</h2>
+<p>Holisticly harness just in time technologies via
+corporate scenarios. Intrinsicly predominate ubiquitous
+outsourcing through an expanded array of functionalities.</p>
+</div>
+</div>
+<a href="#">Learn More &nbsp;<i class="fa fa-angle-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+-->
+		<!-- END CONTENT SLIDER -->
+		<!-- FEATURES -->
+		<section>
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-md-12">
+						<div class="icon-info icon-info-left">
+							<i class="icon icon_book_alt ico-styled-md text-primary"></i>
+							<div class="text">
+								<h2 class="title museosans " style="font-size: 160%;">LATEST
+									PUBLICATIONS</h2>
+
+								<div class="publication banner_news">
+									<b><a href='http://ieeexplore.ieee.org/document/7964096/'>A Service- Oriented Cloud-Based Management System for the Internet-of-Drones</a> </b> <br>A. Koubaˆa, B. Qureshi, M.-F. Sriti, Y. Javed, and E. Tovar<br> <b>17th IEEE International Conference on Autonomous Robot Systems and Competitions (ICARSC 2017)</b>, May 2017
+								</div>
+								<div class="publication banner_news">
+									<b><a href='https://link.springer.com/chapter/10.1007%2F978-3-319-54927-9_8'>ROSLink:Bridging ROS with the Internet-of-Things for Cloud Robotics</a> </b> <br>A.Koubaˆa,M.Alajlan,andB.Qureshi <br> <b>in Springer Book of Robot Operating System (ROS), Volume 2, </b>,May 2017
+								</div>
+								<div class="publication banner_news">
+									<b><a href='http://www.springer.com/us/book/9783319549262'>Robot
+											Operating System (ROS): The Complete Reference (Volume 2)</a>
+									</b><br>Anis Koubaa (Editor) <br> <b>Springer</b>, Volume 2,
+									707 Pages, ISBN 978-3-319-54926-2, 2017 <a
+										href='http://www.springer.com/us/book/9783319549262'> </a>
+								</div>
+
+
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="icon-info icon-info-left">
+							<i class="icon icon_lightbulb ico-styled-md text-primary"></i>
+							<div class="text">
+								<h2 class="title" style="font-size: 160%;">LATEST NEWS</h2>
+
+								<div class="row">
+									<div class="col-lg-6 col-md-6 col-sm-12 col-xm-12 banner_news">
+									<h4>New Grant Attracted</h4>
+										<p>
+											<a href='#'>Nov 1, 2017</a> The RIOTU Lab would like to thank <a href="http://www.gaitech.com/">Gaitech Robotics (China)</a> for the research grant provided to the lab, in the context of R&D collaboration MoU established between Prince Sultan University and Gaitech Robotics. 
+										</p>
+										<br>
+									</div>
+									<div class="col-lg-6 col-md-6 col-sm-12 col-xm-12 banner_news">
+									<h4>Open Positions for Students</h4>
+										<p>
+											<a href='#'>Nov 1, 2017</a> The RIOTU Lab  is looking for talented and skilled students to join the research activities of the lab. 
+											We look for studengs with strong skills in programming and software development. Knowledge of drones, ROS, and Robotics will be an added value. 
+										</p>
+										
+										<br>
+									</div>
+									
+									
+
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+
+					<div class="col-md-12">
+						<div class="icon-info icon-info-left">
+							<i class="icon icon_gift ico-styled-md text-primary"></i>
+							<div class="text">
+								<h2 class="title" style="font-size: 160%;">NEWSLETTER, APRIL
+									2017</h2>
+
+
+
+								<div class="row">
+									<div class="col-lg-8">
+										<a href='resources/newsletter1.pdf'><img
+											src="images/newsletter1.png" width="60%"
+											class="image-responsive center"> </a>
+
+									</div>
+									<div class="col-lg-4"></div>
+								</div>
+
+
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+		</section>
+		<!-- FOOTER -->
+		<?php
+		include_once 'include/footer.php';
+		?>
+		<!-- END FOOTER -->
+		<div class="back-to-top">
+			<a href="#top"><i class="fa fa-chevron-up"></i> </a>
+		</div>
+
+		<!-- END WRAPPER -->
+		<!-- JAVASCRIPT -->
+		<script src="theme/assets/js/jquery-2.1.1.min.js"></script>
+		<script src="theme/assets/js/bootstrap.min.js"></script>
+		<script src="theme/assets/js/plugins/easing/jquery.easing.min.js"></script>
+		<script
+			src="theme/assets/js/plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
+		<script
+			src="theme/assets/js/plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
+		<script src="theme/assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+		<script src="theme/assets/js/bravana.js"></script>
+		<script type="text/javascript"
+			src="theme/assets/js/plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
+		<script type="text/javascript"
+			src="theme/assets/js/plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+		<script type="text/javascript"
+			src="theme/assets/js/plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+		<script type="text/javascript"
+			src="theme/assets/js/plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+		<script type="text/javascript"
+			src="theme/assets/js/plugins/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+		<script type="text/javascript"
+			src="theme/assets/js/plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+		<script type="text/javascript"
+			src="theme/assets/js/plugins/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+		<script type="text/javascript"
+			src="theme/assets/js/plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+
+
+		<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54522619-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+</html>
